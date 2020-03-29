60e1cedf31a508e21f91ab0c7fbe4c823dcd2094
jacobjdong <jacobjdong@gmail.com>
2020-03-28 21:44:12 -0500

    Updates

---
+++ precache-manifest.46ef8dea7bdb45dc973626fc242a2a53.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749896d5503da3b942513bdbeba50ad5",
+    "url": "/hospitalUI/index.html"
+  },
+  {
+    "revision": "aee80f10d73ac198b1d3",
+    "url": "/hospitalUI/static/css/main.80ea0dcd.chunk.css"
+  },
+  {
+    "revision": "7c20e1fbc822bf52dc41",
+    "url": "/hospitalUI/static/js/2.538e604f.chunk.js"
+  },
+  {
+    "revision": "da5012bd8cb93bf58b4e170155124451",
+    "url": "/hospitalUI/static/js/2.538e604f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee80f10d73ac198b1d3",
+    "url": "/hospitalUI/static/js/main.7de2fbf9.chunk.js"
+  },
+  {
+    "revision": "412ea60074d3d9c1086a",
+    "url": "/hospitalUI/static/js/runtime-main.463973e1.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/hospitalUI/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/hospitalUI/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/hospitalUI/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hospitalUI/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/hospitalUI/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/hospitalUI/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/hospitalUI/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/hospitalUI/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/hospitalUI/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "1d794ee6781872ed78d51bfc32856376",
+    "url": "/hospitalUI/static/media/logo.1d794ee6.png"
+  },
+  {
+    "revision": "1d794ee6781872ed78d51bfc32856376",
+    "url": "/hospitalUI/static/media/logo1.1d794ee6.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/hospitalUI/static/media/roboto-700.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/hospitalUI/static/media/roboto-regular.11eabca2.ttf"
+  }
+]);
